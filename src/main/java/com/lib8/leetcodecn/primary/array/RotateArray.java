8c92a12becea30377acdc7e31c8055640f6c3ef4
Lionseun Ling <lionseun@gmail.com>
2020-12-10 00:16:55 +0800

    [finish] 旋转数组

--- src/main/java/com/lib8/leetcodecn/primary/array/RotateArray.java
+++ src/main/java/com/lib8/leetcodecn/primary/array/RotateArray.java
@@ -1,5 +1,7 @@
 package com.lib8.leetcodecn.primary.array;
 
+import java.util.Arrays;
+
 /**
  * <a herf="https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/">旋转数组</a>
  *
@@ -8,20 +10,31 @@ package com.lib8.leetcodecn.primary.array;
  */
 public class RotateArray {
     public void rotate(int[] nums, int k) {
-        if (k <= 0) {
-            return;
-        }
-
-        // TODO: 2020/12/9 测试未过
         int length = nums.length;
-        int eindex = k;
-        for (int i = 0; i < length - 1 ; i++) {
-            int sIndex = length - eindex;
-            int temp = nums[i];
-            nums[i] = nums[sIndex];
-            nums[sIndex] = temp;
-            eindex = eindex - 1;
-            eindex = eindex > 0 ? eindex : k;
+        k %= length;
+        int[] bnums = Arrays.copyOf(nums, length);
+        for (int i = 0; i < length; i++) {
+            nums[(i+k)%length] = bnums[i];
+        }
+    }
+
+    /**
+     * 空间复杂度为O(1)
+     */
+    public void rotate1(int[] nums, int k) {
+        int length = nums.length;
+        k %= length;
+        reverse(nums, 0, length-k-1);
+        reverse(nums, length-k, length-1);
+        reverse(nums, 0, length-1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        int temp;
+        for (int i = 0; i <= (end-start)/2; i++) {
+            temp = nums[start+i];
+            nums[start+i] = nums[end-i];
+            nums[end-i] = temp;
         }
     }
 }
