1ad4289569bd332dc3a008b765087e1f6aa7ee51
Lionseun Ling <lionseun@gmail.com>
2021-07-30 00:57:38 +0800

    insertion & selection sort

--- src/main/java/com/lib8/structure/Sort.java
+++ src/main/java/com/lib8/structure/Sort.java
@@ -27,4 +27,81 @@ public class Sort {
             }
         }
     }
+
+    /**
+     * 插入排序
+     * @param nums
+     */
+    public void insertionSort2(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {  // 跳过第一个值
+            int base = nums[i];
+            for (int j = i-1; j >= 0; j--) {
+                if (base < nums[j]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void insertionSort3(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {  // 跳过第一个值
+            int base = nums[i];
+            int j = i -1;
+            for (; j >= 0; j--) {
+                if (base < nums[j]) {
+                    nums[j+1] = nums[j];
+                } else {
+                    break;
+                }
+            }
+            nums[j+1] = base;
+        }
+    }
+
+    public void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int base = nums[i]; // 缓存插入的值
+            int j = i;
+            for (; j >0 && nums[j-1] > base ; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[j] = base; // 缓存的值，插到数组中
+        }
+    }
+
+    public void selectionSort2(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+    }
+
+    public void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int minIndex = i;
+            for (int j = nums.length -1; j > i; j--) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+    }
+
+
 }
